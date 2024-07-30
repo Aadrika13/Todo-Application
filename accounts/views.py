6e19b301d50050d3ee21c1575b043701f9015e41
Aadrika Jain <aadrikajain13@gmail.com>
2024-07-30 21:27:44 +0530

    register

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login(request):
     return render(request, 'accounts/login.html')
 
 def register(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        new_user = User(
+            username = username
+        )
+
+        new_user.set_password(password)
+
+        new_user.save()
+
+        return redirect('login')
+    
     return render(request, "accounts/register.html")
