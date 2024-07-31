df2a18ed05ad62ef39a0038892f8d976d9928094
Aadrika Jain <aadrikajain13@gmail.com>
2024-07-31 14:38:55 +0530

    Login and some changes in css

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth                                                                                                               # methods in auth model: authenticate() ,login() ,logout()
 
 # Create your views here.
+
 def login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = auth.authenticate(username = username, password = password)
+
+        if user is not None:
+            auth.login(request, user)  # session create ho rha h
+            return redirect('todo')
+        
+        else:
+            return redirect('home')
+
     return render(request, 'accounts/login.html')
+
+#==================================== REGISTER =======================================================================================
 
 def register(request):
 
@@ -22,3 +40,4 @@ def register(request):
         return redirect('login')
     
     return render(request, "accounts/register.html")
+
